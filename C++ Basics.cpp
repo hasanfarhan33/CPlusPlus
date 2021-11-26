1cab75c26923d8a75464261fc82921bc4c5b7d2d
Farhan Hasan <hasanfarhan33@gmail.com>
2021-11-26 16:29:06 +0300

    Add project files.

---
+++ C++ Basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    //ABSOLUTE BASIC
+    /*cout << "Hello World!\n";
+    cout << "Time to learn c++ again" << endl; 
+    cout << "Shouldn't take that long this time." << endl; */
+    
+    system("pause > 0"); //Gets rid of the junk in the console 
+}
+
